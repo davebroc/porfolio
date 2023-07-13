29d5187eb4fd6fb880756d63e7fa0c5c6cccaf5c
David Brockbank <davebrockbank02@gmail.com>
2023-07-13 16:46:32 +1200

    Vite build settings update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'src/main.tsx',
+      },
+    },
+    outDir: 'dist', // Output directory for the build
+    assetsDir: 'assets', // Directory for static assets
+    sourcemap: true, // Generate source maps for easier debugging
+    minify: true, // Enable minification for production build
+    terserOptions: {
+      // Additional options for terser minification
+      compress: {
+        drop_console: true, // Remove console.* statements
+      },
+    },
+  },
+});
