3fd53327ff893145489c670fb9ecde41f73eb216
davebroc <davebrockbank02@gmail.com>
2024-03-05 20:31:29 +1300

    Updated about and experiences

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -1,8 +1,20 @@
 export default [
     {
+        company: "Fast Enterprises",
+        duration: "six months",
+        tasks: `Application support work for Inland Revenue's tax software. Diagnosed and solved bugs using mixture of C# code and configuration. Worked closely with non-technacal IR staff to get requirements for solutions I would then implement. `,
+        title: "Implementation Consultant",
+    },
+    {
+        company: "MTF Finance",
+        duration: "three months",
+        tasks: ``,
+        title: "Software Engineer Intern",
+    },
+    {
         company: "Iris Data Science",
         duration: "three months",
-        tasks: `I designed and built a progressive web application, GoToGrow, within a team of three. 
+        tasks: `Designed and built a progressive web application, GoToGrow, within a team of three. 
         GoToGrow streamlines farming data management, it also gives fertilizer recommendations to farmers, calculates fertilizer mixes, and much more. 
          Some of my tasks included: implementing extensive offline capabilities, workflow design, and front-end development.
          It is currently being used by farmers in NZ, Europe, and USA.`,
@@ -11,13 +23,13 @@ export default [
     {
         company: "University of Otago",
         duration: "four months",
-        tasks: `I help students with practical coding issues in the paper: COSC202 - Software Development.`,
+        tasks: `Helped students with practical coding issues in the paper: COSC202 - Software Development.`,
         title: "200-Level Computer Science Tutor/Demonstrator",
     },
     {
         company: "Brockbank Properties",
         duration: "five years of summer full-time",
-        tasks: "I undertook a broad set of tasks such as installing electric fencing, building frames, concrete pouring, and painting. One summer involved leading a plastering team with minimal supervision.",
+        tasks: "Broad set of responsibilities such as installing electric fencing, building frames, concrete pouring, and painting. One summer involved leading a plastering team with minimal supervision.",
         title: "Plasterer and Labourer",
     }
 
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,22 +1,17 @@
-import { filesPath } from "../util/paths";
-
 export default function About() {
     return (
         <section id='about' className='scroll-mt-28 '>
-            <p>Final year student at University of Otago studying Computer Science and Energy Science/Technology with
-                <a className='underline' href={filesPath + "otago-transcript.pdf"} target='_blank'> excellent grades</a>. I am a fast learner,
-                good at problem solving, and work great in teams.
-            </p>
-            <br></br>
+
             <p>
-                Experience with data science regression and classification modelling. As well as working with C, C++, Python,
-                Java, and JavaScript (React).</p>
-
-            <br></br>
-            <p>Outside of my studies and career I enjoy socializing, snowboarding, the gym, and tramping.</p>
-            <br></br>
-            <p>Interests in the Technology, Energy, and Space Industries.</p>
-
+                Software Engineer who enjoys collaboration and problem solving.
+            </p>
+            <br />
+            <p>
+                In my free time I enjoy training in the gym, making video games in Godot, and socialising.
+                Like to snowboard and tramp when I find the time.
+            </p>
+            <br />
+            <p>Passions for physics, logic, space, and the outdoors.</p>
 
         </section>
     )
